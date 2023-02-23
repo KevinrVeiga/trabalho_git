863c6f5daad6904022ff0e9b6fd1d1c426cfb43a
Nicolly Cardoso <nicollycardoso.ss003@academico.ifsul.edu.br>
2023-02-23 08:43:10 -0300

    "Adicionado funcionalidade digitar nome e imprimir na tela"

--- TRABALHO_GIT_2K/src/Main.java
+++ TRABALHO_GIT_2K/src/Main.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
 
 public class Main {
   
 	 {
 		 System.out.println("Olá MUndo");
-
+ 
+		 Scanner t = new Scanner(System.in);
+		 System.out.println("Kevin Veiga: ");
+		 String a = t.nextLine();
+		 System.out.println("Olá " + a);
+		 System.out.println("Olá Mundo");
 	}
 
 }
